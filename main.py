b550a33bc980da32bac0df48244a3913bb42bf1b
Kris Kafle <kriskafle@tamu.edu>
2026-04-21 20:48:07 -0500

    Test commit again

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 video_path = "489_example.mp4"  # or MOV, whichever you have
 template_path = "ball_template.png"
 
-# test comment for github commit part 2
+# test comment for github commit part 3!!!
 cap = cv2.VideoCapture(video_path)
 
 template = cv2.imread(template_path, 0)
